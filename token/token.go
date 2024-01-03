90cf9a838f17c865d67cafba6fcbcae20fb7aca6
뱀 <54323618+viyamKUN@users.noreply.github.com>
2024-01-04 01:11:57 +0900

    implement tokenizing

--- token/token.go
+++ token/token.go
@@ -32,3 +32,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok // 예약어
+	}
+	return IDENT // 식별자
+}
